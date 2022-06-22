d3542fe627d95edecf281aa0a08258955262a742
Devon Langendoerfer <devon.langendoerfer@konghq.com>
2022-06-21 20:19:05 -0700

    skip python step

--- windows.ps1
+++ windows.ps1
@@ -141,22 +141,22 @@ DownloadAndCheck $env:TEMP\nasm-win64.zip `
 Expand-Archive -Path $env:TEMP\nasm-win64.zip -DestinationPath C:\tools\
 AddToPath C:\tools\nasm-$nasmVersion
 
-# Python3 (do not install via msys2 or the MS store's flavors, this version follows Win32 semantics)
-DownloadAndCheck $env:TEMP\python3-installer.exe `
-                 https://www.python.org/ftp/python/3.9.9/python-3.9.9-amd64.exe `
-                 137d59e5c0b01a8f1bdcba08344402ae658c81c6bf03b6602bd8b4e951ad0714
-# python installer needs to be run as an installer with Start-Process
-RunAndCheckError "$env:TEMP\python3-installer.exe" @("/quiet", "InstallAllUsers=1", "Include_launcher=0", "InstallLauncherAllUsers=0") $true
-AddToPath $env:ProgramFiles\Python39
-AddToPath $env:ProgramFiles\Python39\Scripts
-# Add symlinks for canonical executables expected in a Python environment
-RunAndCheckError "cmd.exe" @("/c", "mklink", "$env:ProgramFiles\Python39\python3.exe", "$env:ProgramFiles\Python39\python.exe")
-RunAndCheckError "cmd.exe" @("/c", "mklink", "$env:ProgramFiles\Python39\python3.9.exe", "$env:ProgramFiles\Python39\python.exe")
-# Upgrade pip
-RunAndCheckError "python.exe" @("-m", "pip", "install", "--upgrade", "pip")
-# Install wheel so rules_python rules will run
-RunAndCheckError "pip.exe" @("install", "wheel")
-RunAndCheckError "pip.exe" @("install", "virtualenv")
+# # Python3 (do not install via msys2 or the MS store's flavors, this version follows Win32 semantics)
+# DownloadAndCheck $env:TEMP\python3-installer.exe `
+#                  https://www.python.org/ftp/python/3.9.9/python-3.9.9-amd64.exe `
+#                  137d59e5c0b01a8f1bdcba08344402ae658c81c6bf03b6602bd8b4e951ad0714
+# # python installer needs to be run as an installer with Start-Process
+# RunAndCheckError "$env:TEMP\python3-installer.exe" @("/quiet", "InstallAllUsers=1", "Include_launcher=0", "InstallLauncherAllUsers=0") $true
+# AddToPath $env:ProgramFiles\Python39
+# AddToPath $env:ProgramFiles\Python39\Scripts
+# # Add symlinks for canonical executables expected in a Python environment
+# RunAndCheckError "cmd.exe" @("/c", "mklink", "$env:ProgramFiles\Python39\python3.exe", "$env:ProgramFiles\Python39\python.exe")
+# RunAndCheckError "cmd.exe" @("/c", "mklink", "$env:ProgramFiles\Python39\python3.9.exe", "$env:ProgramFiles\Python39\python.exe")
+# # Upgrade pip
+# RunAndCheckError "python.exe" @("-m", "pip", "install", "--upgrade", "pip")
+# # Install wheel so rules_python rules will run
+# RunAndCheckError "pip.exe" @("install", "wheel")
+# RunAndCheckError "pip.exe" @("install", "virtualenv")
 
 # 7z only to unpack msys2
 DownloadAndCheck $env:TEMP\7z-installer.exe `
